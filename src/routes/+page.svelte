faf3201b5aaa9a36b2facb0ac885cf0c6753b941
biscotty666 <biscotty666@gmail.com>
2023-01-26 15:02:53 -0700

    Fetch, Process and display csv data

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,30 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  
+  export let data
+
+  // console.log(data);
+
+  const {flowers} = data
+  console.log(flowers);
+</script>
+
+<div class="container">
+  {#each flowers as item}
+    <p>{item.species}</p>
+  {/each}
+</div>
+
+<style>
+	.grid {
+		display: grid;
+		grid-template-columns: repeat(3, 1fr);
+		gap: 2em;
+		padding: 2em;
+	}
+	img {
+		height: 300px;
+		width: 100%;
+		object-fit: cover;
+		object-position: center;
+	}
+</style>
